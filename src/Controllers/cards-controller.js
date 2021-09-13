dcc4f1b5dd55bece4cd43ab86cbc90f1abba6573
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2021-09-13 10:57:21 +0200

    feature of card

---
+++ src/Controllers/cards-controller.js
@@ -0,0 +1,49 @@
+import { Card } from '../db/models';
+
+
+class CardController{
+
+    static async createCard(req, res){
+
+        try {
+            const { cardId } = req.body;
+
+           
+           
+            const newCard = await Card.create({
+                cardId,
+               
+            });
+            return res.status(201).json({
+                status: 201,
+                message: 'A new card have been added',
+                data: newCard
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: 500,
+                error: error.message
+            });
+        }
+
+    }
+
+    static async allCards(req, res){
+        try {
+            const allCards = await Card.findAll();
+            return res.status(200).json({
+                status: 200,
+                message: 'Add cards were retrieved successfully',
+                data: allCards
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status:500,
+                error: error.message
+            });
+        }
+    }
+
+}
+
+export default CardController;
