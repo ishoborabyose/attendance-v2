830732d031eed03805940339af700412548ac52c
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2021-10-15 09:56:42 +0200

    making relationship

--- src/db/models/card.js
+++ src/db/models/card.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // Card.belongsTo(models.Attendee, {foreignKey: 'cardId', onDelete: 'CASCADE'})
-      // Card.belongsTo(models.Employees, {foreignKey: 'cardId', onDelete: 'CASCADE'})
+      Card.belongsTo(models.Employees, {foreignKey: 'cardId', onDelete: 'CASCADE'})
       
     }
   };
