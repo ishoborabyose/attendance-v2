a6c32e7af3b31958414aef97f509edc80fbd4a19
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-01-26 09:08:23 +0200

    some modification

--- src/Controllers/attendee-controller.js
+++ src/Controllers/attendee-controller.js
@@ -1,77 +1,61 @@
-import { 
-    Attendee,
-    Employees,
-    Card
- } from '../db/models';
+import { Attendee, Employees, Card } from "../db/models";
 
-
-
-class AttendeeController{
-
-    static async createAttendee(req, res){
-
-        try {
-            
-            const { cardId  } = req.query;
-            Card.findOne({
-                where: { cardId }
-            })
-            .then((readCard)=>{
-                if(readCard){
-                  Employees.findOne({
-                        where: {
-                            cardId
-                        }
-                    })
-                    .then((readEmployee) =>{
-                        if(readEmployee) {
-                            const selectedEmployee = {
-                                cardId: readEmployee.cardId,
-                                employeeId: readEmployee.employeeId,
-                                name: readEmployee.firstName,
-        
-                            };
-                              Attendee.create(selectedEmployee)
-                              return res.status(200).json({
-                                  status: 200,
-                                  message: 'You successfully attended',
-                                  data: selectedEmployee
-                              })
-                            
-                        } else {
-                            res.json({error: 'Access denied'});
-                        }
-                    })
-                }
-            })
-    
-        
-            
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                error: error.message
-            });
-        }
-
-    }
-
-    static async allAttendee(req, res){
-        try {
-            const allAttendee = await Attendee.findAll();
-            return res.status(200).json({
+class AttendeeController {
+  static async createAttendee(req, res) {
+    try {
+      const { cardId } = req.query;
+      Card.findOne({
+        where: { cardId },
+      }).then((readCard) => {
+        if (readCard) {
+          Employees.findOne({
+            where: {
+              cardId,
+            },
+          }).then((readEmployee) => {
+            if (readEmployee) {
+              const selectedEmployee = {
+                cardId: readEmployee.cardId,
+                employeeId: readEmployee.employeeId,
+                name: readEmployee.firstName,
+              };
+              Attendee.create(selectedEmployee);
+              return res.status(200).json({
                 status: 200,
-                message: 'Add attendee were retrieved successfully',
-                data: allAttendee
-            });
-        } catch (error) {
-            return res.status(500).json({
-                status:500,
-                error: error.message
-            });
+                message: "You successfully attended",
+                data: selectedEmployee,
+              });
+            } else {
+              res.json({ error: "Access denied" });
+            }
+          });
         }
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 500,
+        error: error.message,
+      });
     }
+  }
 
+  static async allAttendee(req, res) {
+    try {
+      const allAttendee = await Attendee.findAll({
+        order: [["id", "DESC"]],
+      });
+      return res.status(200).json({
+        status: 200,
+        message: "Add attendee were retrieved successfully",
+        data: allAttendee,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 500,
+        error: error.message,
+      });
+    }
+  }
 }
 
 export default AttendeeController;
--- src/Controllers/employee-controller.js
+++ src/Controllers/employee-controller.js
@@ -1,80 +1,71 @@
-import {
-    Employees
-} from '../db/models';
-
+import { Employees } from "../db/models";
 
 class EmployeeController {
+  static async createEmployee(req, res) {
+    try {
+      const {
+        employeeId,
+        cardId,
+        firstName,
+        lastName,
+        gender,
+        department,
+        position,
+        PhoneNumber,
+        email,
+      } = req.body;
 
-    static async createEmployee(req, res) {
+      const employeeExist = await Employees.findOne({
+        where: { employeeId },
+      });
 
-        try {
-            const {
-                employeeId,
-                cardId,
-                firstName,
-                lastName,
-                gender,
-                department,
-                position,
-                PhoneNumber,
-                email,
-              
-            } = req.body;
+      if (employeeExist) {
+        return res.status(403).json({
+          status: 403,
+          error: "This employee arleady exist",
+        });
+      }
 
-            const employeeExist = await Employees.findOne({
-                where: { employeeId }
-            })
+      const newEmployee = await Employees.create({
+        employeeId,
+        cardId,
+        firstName,
+        lastName,
+        gender,
+        department,
+        position,
+        PhoneNumber,
+        email,
+      });
 
-            if(employeeExist){
-                return res.status(403).json({
-                    status: 403,
-                    error: 'This employee arleady exist'
-                })
-            }
-
-            const newEmployee = await Employees.create({
-                employeeId,
-                cardId,
-                firstName,
-                lastName,
-                gender,
-                department,
-                position,
-                PhoneNumber,
-                email,
-            });
-            
-            return res.status(201).json({
-                status: 201,
-                message: 'A new employee have been added',
-                data: newEmployee
-            });
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                error: error.message,
-
-            });
-        }
-
+      return res.status(201).json({
+        status: 201,
+        message: "A new employee have been added",
+        data: newEmployee,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 500,
+        error: error.message,
+      });
     }
+  }
 
-    static async allEmployee(req, res) {
-        try {
-            const allEmployee = await Employees.findAll();
-            return res.status(200).json({
-                status: 200,
-                message: 'Add employees were retrieved successfully',
-                data: allEmployee
-            });
-        } catch (error) {
-            return res.status(500).json({
-                status: 500,
-                error: error.message
-            });
-        }
+  static async allEmployee(req, res) {
+    try {
+      const allEmployee = await Employees.findAll();
+      return res.status(200).json({
+        status: 200,
+        message: "Add employees were retrieved successfully",
+        data: allEmployee,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: 500,
+        error: error.message,
+      });
     }
-
+  }
 }
 
 export default EmployeeController;
